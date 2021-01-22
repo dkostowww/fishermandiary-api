b71909d8bed1326f8f50a797c82478071354890f
Dimiter Kostov <dimiter@netreact.com>
2021-01-22 15:51:08 +0200

    Init trips table

--- migrations/m210122_134122_create_trips_table.php
+++ migrations/m210122_134122_create_trips_table.php
@@ -12,9 +12,23 @@ class m210122_134122_create_trips_table extends Migration
      */
     public function safeUp()
     {
+
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
         $this->createTable('{{%trips}}', [
             'id' => $this->primaryKey(),
-        ]);
+            'user_id' => $this->string()->notNull(),
+            'location' => $this->string(32)->notNull(),
+            'catch_amount' => $this->string()->notNull(),
+            'trip_description' => $this->string(),
+            'created_at' => $this->date()->notNull(),
+            'updated_at' => $this->dateTime(),
+        ], $tableOptions);
+
     }
 
     /**
